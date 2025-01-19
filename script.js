a68e87c256df636e27fe8afe138db878ef758594
charlesassoi <charlesassoi07@gmail.com>
2025-01-19 04:38:29 +0000

    new

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
 const canvas =document.getElementById("canvas1");
 const ctx= canvas.getContext("2D");
 console.log(ctx);
+
+const CANVAs_WIDTH=canvas.width=600;
+const CANVAs_HEIGHT=canvas.heght=600;
+const playerImage= new Image();
+playerImage.src="shadow_dog.png";
+function animate {
+    ctx.clearRect(0,0,CANVAs_WIDTH,CANVAs_HEIGHT);
+    ctx.fillRect(50,50,100,100);
+    requestAnimationFrame(animate);
+}
